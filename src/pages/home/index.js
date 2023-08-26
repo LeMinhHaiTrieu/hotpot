df1f02537bd4e027f13db986d68b1379782188f0
trieu.le <trieulmh@tenxtenx.com>
2023-08-26 11:56:56 +0700

    update: fast link home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,45 +1,86 @@
 import { useState, useEffect } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { styled, keyframes } from 'styled-components';
-import { Mousewheel, Pagination } from 'swiper/modules';
-import AOS from "aos";
-import "aos/dist/aos.css";
-import Slide3 from '../../components/Slide3';
-import TopNav from '../../components/TopNav';
-import MTopNav from '../../components/MTopNav';
-import MSlide2 from '../../components/MSlide2';
-import MSlide3 from '../../components/MSlide3';
-import FooterNew from '../../components/FooterNew';
-import Slide1New from '../../components/Slide1New';
-import Slide2New from '../../components/Slide2New';
-import ArrowDown from '../../asset/images/arrow-down.png';
-import { WrapHome } from './styled';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Link } from 'react-router-dom'
+import { Mousewheel, Pagination } from 'swiper/modules'
+import AOS from "aos"
+import "aos/dist/aos.css"
+import Slide3 from '../../components/Slide3'
+import TopNav from '../../components/TopNav'
+import MTopNav from '../../components/MTopNav'
+import MSlide2 from '../../components/MSlide2'
+import MSlide3 from '../../components/MSlide3'
+import FooterNew from '../../components/FooterNew'
+import Slide1New from '../../components/Slide1New'
+import Slide2New from '../../components/Slide2New'
+import ArrowDown from '../../asset/images/arrow-down.png'
+import IconOrderOnlineGray from '../../asset/svg/icon_order_online_gray.svg'
+import IconWaitList from '../../asset/svg/icon_wait_list.svg'
+import IconOrderOnlineGrayWhite from '../../asset/svg/icon_order_online_white.svg'
+import IconWaitListWhite from '../../asset/svg/icon_wait_list_white.svg'
+import IconOrderOnlineRed from '../../asset/svg/icon_order_online.svg'
 
-const CircleDownAnimation = keyframes`
-  0% {
-    bottom: 1%;
-  }
-  50% {
-    bottom: 3%;
-  }
-  100% {
-    bottom: 1%;
-  }
-`;
+import {
+  WrapHome,
+  CircleDownSlide,
+  WrapFastLink,
+  WrapFastItem,
+  LabelFastLink,
+  IconFastLink
+} from './styled';
 
-const CircleDownSlide = styled.div`
-  position: absolute;
-  bottom: 2%;
-  width: 30px;
-  left: 50%;
-  cursor: pointer;
-  margin-left: -15px;
-  animation: ${CircleDownAnimation} 1s infinite;
-  z-index: 999;
-  img {
-    width: 30px;
-  }
-`
+const dataFastLink = [
+  {
+    label: "Wait List",
+    isTargetBlank: true,
+    icon: IconWaitList,
+    iconHover: IconWaitListWhite,
+    link: "https://www.google.com/"
+  },
+  {
+    label: "Make Reservation",
+    isTargetBlank: false,
+    icon: IconOrderOnlineGray,
+    iconHover: IconOrderOnlineGrayWhite,
+    link: '/reservation',
+    isReservation: true,
+  },
+]
+
+const ContentLink = ({label, icon, iconHover, isReservation }) => {
+  return (
+    <>
+      <LabelFastLink className="label">
+        <span>{label}</span>
+        <img src={iconHover} alt="" />
+      </LabelFastLink>
+      <IconFastLink className={`${isReservation && "ring"} icon`}>
+        {!isReservation && <img src={icon} />}
+        {isReservation && <img src={IconOrderOnlineRed} />}
+      </IconFastLink>
+    </>
+  )
+}
+
+const FastLink = () => {
+  return (
+    <WrapFastLink>
+      {dataFastLink.map(item =>
+        <WrapFastItem key={item.label}>
+          {item.isTargetBlank &&
+            <a className='d-flex' target="_blank" href={item.link}>
+              <ContentLink {...item} />
+            </a>
+          }
+          {!item.isTargetBlank &&
+            <Link to={item.link} className='d-flex'>
+              <ContentLink {...item} />
+            </Link>
+          }
+        </WrapFastItem>
+      )}
+    </WrapFastLink>
+  )
+}
 
 function App() {
   const [activeSlide, setActiveSlide] = useState(0);
@@ -47,9 +88,7 @@ function App() {
   const [swiper, setSwiper] = useState(null);
 
   const handleOnSlideChange = (e) => {
-    setTimeout(() => {
-      setActiveSlide(e.activeIndex)
-    }, 1500)
+    setActiveSlide(e.activeIndex)
   }
 
   const nexToSlide = () => {
@@ -84,12 +123,12 @@ function App() {
             mousewheel={true}
             pagination={{
               clickable: true,
+              
             }}
             modules={[Mousewheel, Pagination]}
             className="mySwiper"
             onSlideChange={handleOnSlideChange}
             onSwiper={(s) => {
-              console.log("initialize swiper", s);
               setSwiper(s);
             }}
           >
@@ -98,21 +137,23 @@ function App() {
             <SwiperSlide><Slide3 activeSlide={activeSlide} /></SwiperSlide>
             <SwiperSlide className='footer-slide'><FooterNew /></SwiperSlide>
           </Swiper>
-          {activeSlide == 3 &&
+          {/* {activeSlide !== 3 &&
             <CircleDownSlide onClick={nexToSlide}>
               <img src={ArrowDown} alt="" />
             </CircleDownSlide>
-          }
+          } */}
         </>
       }
       {isMobile &&
         <>
           <MTopNav />
+          <Slide1New activeSlide={activeSlide} />
           <MSlide2 />
           <MSlide3 />
           <FooterNew />
         </>
       }
+      <FastLink />
     </WrapHome>
   );
 }
